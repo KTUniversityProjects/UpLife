c9883b9b2a8c71e88fc469bf5424de2aa1bc89e3
benmandr <bmandravickas@gmail.com>
2019-09-15 19:20:55 +0300

    Added backend logic. Connected to MongoDB. Prepared REST requests

---
+++ Server/data.js
@@ -0,0 +1,13 @@
+// /backend/data.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema(
+  {
+    id: Number,
+    message: String
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Data", DataSchema);
